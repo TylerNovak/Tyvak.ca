177965de20420474a17cd8769464cf9042627870
TylerNovak <52401256+TylerNovak@users.noreply.github.com>
2022-09-29 03:03:39 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -244,7 +244,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 	animateSpan.typed({
-		strings: [" Club DJ", "n Event DJ", " Producer"],
+		strings: ["Club DJ", "Event DJ", "Producer"],
 		loop: true,
 		startDelay: 1e3,
 		backDelay: 2e3
